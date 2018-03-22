fd8267f8f8169405ea537bca971b480db94a848c
MattMoonSchott <matt.p.moon@gmail.com>
2018-03-22 16:02:52 -0400

    Update Spotify.py
    Add API Key

--- Spotify.py
+++ Spotify.py
@@ -3,8 +3,9 @@ import requests, json, urllib
 def getTracks(events):
     for event in events:
         name = urllib.parse.quote(event.name)
+        header = {"Authorization" : "Bearer 44f10b7bf4cc45a39a55a7d3d96bc7de"}
         request = 'https://api.spotify.com/v1/search?q=' + name + '&type=artist'
-        results = json.loads(requests.get(request).text)['artists']
+        results = json.loads(requests.get(request, headers=header).text)['artists']
         if results['total'] > 0:
             id = results['items'][0]['id']
             request = 'https://api.spotify.com/v1/artists/'+ id +'/top-tracks?country=US'
